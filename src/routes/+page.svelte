37b7bf7ab7eab0fc3792fb1214df78486c111b7e
Carlos <carlos@carlosmoshu.com>
2023-01-11 11:00:44 +0100

    Refactor host counter filter to utils.ts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,78 +1,67 @@
 <script lang="ts">
-	/* Modules */
-	import Host from './Host.svelte';
+	import HostCard from './HostCard.svelte';
 	import HostInfo from './HostInfo.svelte';
-	/* Types */
-	import type { HostType } from '../types';
-	/* Functions */
-	import postFunctions from '../post';
+	import ZabbixAPI from '../post';
 
-	const devonly_ApiTokenKey = '712d00c487267e61984018e1528fa4b735819c9666a3d2cf3d628eee66a1185b';
+	import { getDevicesCount } from './utils';
 
-	let hosts: Array<HostType> = [];
-	
-	postFunctions.login().then((response: any) => {
-			//let auth = response.data.result; //Use this on production environment
-			postFunctions.getHosts(devonly_ApiTokenKey).then((response) => {
-					hosts = response.data.result;
-					console.log(hosts);
-					devices = {
-						count: hosts.length,
-						online: hosts.filter(
-							(host) =>
-								host.items.filter(
-									(item) => (item.name === 'Zabbix agent ping' && item.lastvalue === '1') || (item.name === 'ICMP ping' && item.lastvalue === '1')
-								).length > 0
-						).length,
-						offline: hosts.filter(
-							(host) =>
-								host.items.filter(
-									(item) => (item.name === 'Zabbix agent ping' && item.lastvalue === '0') || (item.name === 'ICMP ping' && item.lastvalue === '0')
-								).length > 0
-						).length
-					};
-				})
-				.catch((error) => {
-					console.log('error:', error);
-				});
-		});
+	import type { ZabbixHost, DeviceCounters } from '../types';
 
-	let shallShowModal = false;
-	let currentHost: HostType | null = null;
+	const PRE_APIKEY: string = '712d00c487267e61984018e1528fa4b735819c9666a3d2cf3d628eee66a1185b';
 
-	function showModal(host: HostType) {
+	let loadedHosts: Array<ZabbixHost> = [];
+	let shallShowModal: boolean = false;
+	let currentHost: ZabbixHost = {
+		name: '',
+		active_available: '',
+		interfaces: [],
+		items: [],
+		groups: []
+	};
+	let devices: DeviceCounters = {
+		total: 0,
+		available: 0,
+		unavailable: 0
+	};
+	ZabbixAPI.login().then((_response) => {
+		//let auth = response.data.result; //Use this on production environment
+		ZabbixAPI.getHosts(PRE_APIKEY)
+			.then((response) => {
+				loadedHosts = response.data.result;
+				devices = getDevicesCount(loadedHosts);
+			})
+			.catch((error) => {
+				console.log('error:', error);
+			});
+	});
+
+	function showModal(host: ZabbixHost) {
 		shallShowModal = true;
 		currentHost = host;
 	}
-	let devices = {
-		count: 0,
-		online: 0,
-		offline: 0
-	};
 </script>
 
 <svelte:head>
-	<title>Home</title>
 	<meta name="description" content="Trison Zabbix" />
 </svelte:head>
 
 <section>
 	<div id="modal">
 		{#if shallShowModal}
-			<HostInfo host={currentHost} />
+			<HostInfo zabbixHost={currentHost} />
 		{/if}
 	</div>
 	<div class="head-data">
-		<div class="device-count">Devices: {devices.count}</div>
+		<div class="device-count">Devices: {devices.total}</div>
 		<div class="status-count">
-			<span class="offline-bg">{devices.offline}</span>
-			<span class="online-bg">{devices.online}</span>
+			<span class="offline-bg">{devices.unavailable}</span>
+			<span class="online-bg">{devices.available}</span>
 		</div>
 	</div>
 	<div class="hosts">
-		{#each hosts as host}
+		{#each loadedHosts as host}
 			<div class="host" on:click={() => showModal(host)} on:keydown={() => showModal(host)}>
-				<Host {host} />
+				<HostCard zabbixHost={host} />
 			</div>
 		{/each}
 	</div>
@@ -85,12 +74,10 @@
 		align-items: center;
 		padding: 2rem;
 	}
-
 	.device-count {
 		font-size: 1.5rem;
 		font-weight: 600;
 	}
-
 	.status-count {
 		display: flex;
 	}
@@ -102,14 +89,12 @@
 		text-align: center;
 		padding: 0.5rem 0.5rem;
 	}
-
 	.hosts {
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
 		padding: 0 2rem;
 	}
-
 	#modal {
 		position: sticky;
 		top: 0;
