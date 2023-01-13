55d17de8b0870b24bc8aa6c6b13d6c287e6a6cb6
Carlos <carlos@carlosmoshu.com>
2023-01-13 08:58:39 +0100

    Export individual functions

--- src/post.ts
+++ src/post.ts
@@ -3,33 +3,51 @@ import axios from 'axios';
 const API_url = 'http://20.229.182.95:9080//api_jsonrpc.php';
 const PRO_API_url = 'http://localhost/api_jsonrpc.php';
 
-const post = {
-	login: () => {
-		return axios.post(API_url, {
-			jsonrpc: '2.0',
-			method: 'user.login',
-			params: {
-				user: 'Admin',
-				password: 'zabbix'
-			},
-			id: 1,
-			auth: null
-		});
-	},
-	getHosts: (token: string) => {
-		return axios.post(API_url, {
-			jsonrpc: '2.0',
-			method: 'host.get',
-			params: {
-				output: ['active_available', 'name'],
-				selectInterfaces: ['ip'],
-				selectItems: ['name', 'lastvalue'],
-				selectGroups: ['name']
-			},
-			auth: token,
-			id: 1
-		});
-	}
-};
+function login () {
+	return axios.post(API_url, {
+		jsonrpc: '2.0',
+		method: 'user.login',
+		params: {
+			user: 'Admin',
+			password: 'zabbix'
+		},
+		id: 1,
+		auth: null
+	});
+}
 
+function getHosts(token: string) {
+	return axios.post(API_url, {
+		jsonrpc: '2.0',
+		method: 'host.get',
+		params: {
+			output: ['active_available', 'name'],
+			selectInterfaces: ['ip'],
+			selectItems: ['name', 'lastvalue'],
+			selectGroups: ['name']
+		},
+		auth: token,
+		id: 1
+	});
+}
+
+function getHostGroups(token: string) {
+	return axios.post(API_url, {
+		jsonrpc: '2.0',
+		method: 'hostgroup.get',
+		params: {
+			output: ['name']
+		},
+		auth: token,
+		id: 1
+	});
+}
+
+const post = {
+	login: login,
+	getHosts: getHosts,
+	getHostGroups: getHostGroups
+}
+
+export { login, getHosts, getHostGroups };
 export default post;
