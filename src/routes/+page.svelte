6801e965cc6d41f51723705f4e0058f16b06dba0
Carlos <carlos@carlosmoshu.com>
2023-01-18 09:53:07 +0100

    Offline devices page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,7 +42,8 @@
 				.then((response) => {
 					console.log('hosts loaded!', response.data.result);
 					loadedData.hosts = response.data.result;
-					loadedData.counters = get_deviceCounters(response.data.result);
+					loadedData.hosts = filterOffline(loadedData.hosts);
+					loadedData.counters = get_deviceCounters(loadedData.hosts);
 				})
 				.catch(catch_error);
 			ZabbixApiPost.getHostGroups('712d00c487267e61984018e1528fa4b735819c9666a3d2cf3d628eee66a1185b')
@@ -66,6 +67,7 @@
 			ZabbixApiPost.getHosts(authToken)
 				.then((response) => {
 					loadedData.hosts = response.data.result;
+					loadedData.hosts = filterOffline(loadedData.hosts);
 					loadedData.counters = get_deviceCounters(loadedData.hosts);
 				})
 				.catch((error) => {
@@ -82,6 +84,20 @@
 				});
 		}
 	}
+	function filterOffline(hosts: Array<ZabbixHost>): Array<ZabbixHost> {
+		let filteredHosts: Array<ZabbixHost> = [];
+		hosts.forEach((host) => {
+			host.items.forEach((item) => {
+				if (item.name === 'Zabbix agent ping' && item.lastvalue === '0') {
+					filteredHosts.push(host);
+				}
+				else if (item.name === 'ICMP ping' && item.lastvalue === '0') {
+					filteredHosts.push(host);
+				}
+			});
+		});
+		return filteredHosts;
+	}
 	//#endregion
 </script>
 
@@ -95,87 +111,37 @@
 		{/if}
 	</div>
 	<div id="head-data">
-		<div id="device-count">Devices: {loadedData.counters.total}</div>
-		<div id="select-filter-hostgroup">
-			Filter by group:
-			<select id="group-selector" on:change={on__selectOptionChanged}>
-				<option value="all">All</option>
-				{#each loadedData.groups as group}
-					{#if !uninterestedGroups.includes(group.name)}
-						<option value={group.name}>{group.name}</option>
-					{/if}
-				{/each}
-			</select>
-		</div>
-		<div id="status-count">
-			<span class="offline-bg">{loadedData.counters.unavailable}</span>
-			<span class="online-bg">{loadedData.counters.available}</span>
-		</div>
+		<div id="device-count">Offline Devices: {loadedData.counters.total}</div>
 	</div>
 	<div id="hosts">
-		{#each loadedData.hosts as host}
-			<div
-				class="host"
-				on:click={() => on__hostCardClicked(host)}
-				on:keydown={() => on__hostCardClicked(host)}
-			>
-				<HostCard zabbixHost={host} />
-			</div>
-		{/each}
+		{#if loadedData.hosts.length === 0}
+			<div id="no-hosts">No hosts found</div>
+		{:else}
+			{#each loadedData.hosts as host}
+				<div
+					class="host"
+					on:click={() => on__hostCardClicked(host)}
+					on:keydown={() => on__hostCardClicked(host)}
+				>
+					<HostCard zabbixHost={host} />
+				</div>
+			{/each}
+		{/if}
 	</div>
 </section>
 
 <style lang="css" scoped>
 	#head-data {
 		display: flex;
-		justify-content: space-between;
+		justify-content: left;
 		align-items: center;
 		padding: 1rem;
 		margin-top: 2rem;
 		margin-bottom: 1rem;
 	}
-	#select-filter-hostgroup {
-		display: flex;
-		align-items: center;
-		font-family: var(--primary-font);
-		letter-spacing: 0.1rem;
-	}
-	#group-selector {
-		-webkit-appearance: none;
-		-moz-appearance: none;
-		appearance: none;
-		background: transparent;
-		border: none;
-		border-bottom: 1px solid var(--optional-color-5a);
-		color: var(--optional-color-5a);
-		font-family: var(--primary-font);
-		font-size: 1rem;
-		font-weight: 600;
-		letter-spacing: 0.1rem;
-		margin-left: 0.5rem;
-		background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path fill='%23fff' d='M7 10l5 5 5-5z'/><path fill='none' d='M0 0h24v24H0z'/></svg>");
-		background-repeat: no-repeat;
-		background-position: right 0.5rem top 50%;
-		background-size: 1.5rem;
-		text-align: center;
-		text-align-last: center;
-		min-width: 10rem;
-	}
 	#device-count {
 		font-size: 1rem;
 		font-weight: 600;
-		font-family: var(--primary-font);
-	}
-	#status-count {
-		display: flex;
-	}
-	#status-count span {
-		display: flex;
-		align-items: center;
-		font-size: 1rem;
-		font-weight: 600;
-		text-align: center;
-		padding: 0.5rem 0.5rem;
 		font-family: var(--primary-font);
 	}
 	#hosts {
@@ -215,22 +181,8 @@
 			margin-top: 2rem;
 			font-size: 0.6rem;
 		}
-		#select-filter-hostgroup {
-			margin-bottom: 1rem;
-			margin-top: 1rem;
-		}
-		#group-selector {
-			min-width: 5rem;
-		}
 		#device-count {
 			font-size: 0.6rem;
-		}
-		#status-count span {
-			font-size: 0.6rem;
-		}
-		#status-count {
-			/* at right */
-			align-items: center;
 		}
 		.host {
 			width: 100%;
